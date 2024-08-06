5958e89221919e55c1e600bd7a0b5f714a164b8e
ashish-509 <ashish.078bei006@tcioe.edu.np>
2024-08-06 21:32:07 +0545

    be

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 const port = process.env.PORT || 3000 || 4000;
 
 app.use(cors({
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
